afd1a95cf908780940b3b861b40cc243973bf5d9
gsedualdo <edu.goes10@gmail.com>
2024-09-24 21:37:48 -0300

    prisma and mongodb first install

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+
+    users.push(req.body)
+
+    res.status(201).json(req.body)
+
+})
+
+app.get('/usuarios', (req, res) => {
+    res.status(200).json(users)
+})
+
+app.listen(3000)
+
+/*
+userDB: eduardo
+senha: LONHHnC9q2dD41x8
+
+*/
